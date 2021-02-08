9f128303db620195dcc314e5930d7e20961a5536
Will Goldie <wgoldie@gmail.com>
2021-02-07 22:15:03 -0500

    work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::io::Read;
+use std::ops::Range;
 use std::io::Write;
 use std::io;
 use std::cmp::Ordering;
@@ -52,6 +53,15 @@ enum AddressOrRange<T> {
     AddressRange(T, T),
 }
 
+impl AddressOrRange<usize> {
+    fn to_range(self) -> Range<usize> {
+        match self {
+            AddressOrRange::Address(a) => (a..(a+1)),
+            AddressOrRange::AddressRange(a, b) => a..b,
+        }
+    }
+}
+
 enum NedCommand {
     Append(Address),
     Insert(Address),
@@ -62,14 +72,39 @@ enum NedCommand {
     Quit
 }
 
-fn parse_leading_address(command_str: &str) -> (Option<AddressOrRange<Address>>, &str) {
+fn parse_address(command_str: &str) -> (Option<Address>, &str) {
     match command_str.chars().nth(0) {
-        Some('.') => (Some(AddressOrRange::Address(Address::Current)), &command_str[1..]),
-        Some('$') => (Some(AddressOrRange::Address(Address::Last)), &command_str[1..]),
-        Some('-') => (Some(AddressOrRange::Address(Address::NthPrevious(1))), &command_str[1..]),
-        Some('+') => (Some(AddressOrRange::Address(Address::NthNext(1))), &command_str[1..]),
+        Some('.') => (Some(Address::Current), &command_str[1..]),
+        Some('$') => (Some(Address::Last), &command_str[1..]),
+        Some('-') => (Some(Address::NthPrevious(1)), &command_str[1..]),
+        Some('+') => (Some(Address::NthNext(1)), &command_str[1..]),
+        Some(c) => {
+            if c.is_digit(10) {
+                let numeric_string = command_str.chars()
+                    .take_while(|c| c.is_digit(10))
+                    .collect::<String>();
+                let n = numeric_string.parse::<usize>().unwrap();
+                (Some(Address::Nth(n)), &command_str[numeric_string.len()..])
+            } else {
+                (None, command_str)
+            }
+        }
+        _ => (None, command_str)
+    }
+}
+
+fn parse_address_range_shorthand(command_str: &str) -> (Option<AddressOrRange<Address>>, &str) {
+    match command_str.chars().nth(0) {
         Some(';') => (Some(AddressOrRange::AddressRange(Address::Current, Address::Last)), &command_str[1..]),
         _ => (None, command_str)
+    }
+}
+
+fn parse_leading_address(command_str: &str) -> (Option<AddressOrRange<Address>>, &str) {
+    if let (Some(addr), remainder) = parse_address(command_str) {
+        (Some(AddressOrRange::Address(addr)), remainder)
+    } else {
+        parse_address_range_shorthand(command_str)
     }
 }
 
@@ -88,12 +123,26 @@ fn parse_command(command_str: &str, addr: Option<AddressOrRange<Address>>) -> Op
 }
 
 fn parse_command_str(command_str: &str) -> Option<NedCommand> {
-    let (addresses, remainder) = parse_leading_address(command_str);
-    let (command, remainder) = parse_command(remainder, addresses)?;
+    let (aorr_1, remainder) = parse_leading_address(command_str);
+    let (aorr, remainder) = match (aorr_1, remainder.chars().nth(0)) {
+        (Some(AddressOrRange::Address(addr)), Some(',')) => {
+            let (aorr_2, remainder_2) = parse_address(command_str);
+
+            (Some(AddressOrRange::AddressRange(addr, aorr_2.unwrap())), remainder_2)
+        },
+        (x, _) => (x, remainder),
+    };
+
+    let (command, _remainder) = parse_command(remainder, aorr)?;
     Some(command)
 }
 
-fn run_command(state: &mut NedState, command_str: &str) -> Option<()> {
+enum CommandResult {
+    Noop,
+    Quit,
+}
+
+fn run_command(state: &mut NedState, command_str: &str) -> Option<CommandResult> {
     match parse_command_str(command_str) {
         Some(NedCommand::Append(addr)) => {
             let input = run_input();
@@ -101,9 +150,42 @@ fn run_command(state: &mut NedState, command_str: &str) -> Option<()> {
                 // TODO this clone should be replaced with lifetime
                 state.line_buffer.insert(state.reify_address(&addr)? + i, line.to_owned());
             }
-            Some(())
+            Some(CommandResult::Noop)
         },
-        Some(_) => { println!("not implemented"); Some(()) },
+        Some(NedCommand::Insert(addr)) => {
+            let input = run_input();
+            for (i, line) in input.iter().enumerate() { 
+                // TODO this clone should be replaced with lifetime
+                state.line_buffer.insert(state.reify_address(&addr)? - 1 + i, line.to_owned());
+            }
+            Some(CommandResult::Noop)
+        },
+        Some(NedCommand::Print(addr_or_range)) => {
+            let range = state.reify_address_or_range(&addr_or_range)?.to_range();
+            for line in state.line_buffer.get(range).unwrap() {
+                println!("{}", line);
+            }
+            Some(CommandResult::Noop)
+        },
+        Some(NedCommand::PrintLn(addr_or_range)) => {
+            let range = state.reify_address_or_range(&addr_or_range)?.to_range();
+            for i in range {
+                println!("{} {}", i, state.line_buffer.get(i).unwrap());
+            }
+            Some(CommandResult::Noop)
+        },
+        Some(NedCommand::Change(addr_or_range)) => {
+            let range = state.reify_address_or_range(&addr_or_range)?.to_range();
+            let input = run_input();
+            state.line_buffer = state.line_buffer.splice(range, input).collect();
+            Some(CommandResult::Noop)
+        }
+        Some(NedCommand::Delete(addr_or_range)) => {
+            let range = state.reify_address_or_range(&addr_or_range)?.to_range();
+            state.line_buffer = state.line_buffer.splice(range, vec![]).collect();
+            Some(CommandResult::Noop)
+        },
+        Some(NedCommand::Quit) => Some(CommandResult::Quit),
         None => None,
     }
 }
@@ -146,7 +228,12 @@ fn run_editor(buffer: &str) {
         print_flush("> ");
         let mut command = String::new();
         io::stdin().read_line(&mut command).expect("IO");
-        run_command(&mut state, &command);
+        let result = run_command(&mut state, &command);
+        match result {
+            Some(CommandResult::Quit) => break,
+            Some(CommandResult::Noop) => (),
+            None => println!("?"),
+        }
         println!("{:?}", state.line_buffer);
     }   
 }
